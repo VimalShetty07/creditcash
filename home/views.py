44cf06eaaf41f1048206adbbdb756bfeaa52b031
Vimal <shettyvimal07@gmail.com>
2022-08-30 11:49:41 +0530

    Login page updated

--- home/views.py
+++ home/views.py
@@ -11,19 +11,18 @@ def index(request):
         Username = request.POST.get("uname")
         pass1 = request.POST.get("psw")
         
-
         user = authenticate(username=Username, password=pass1)
-
+        
         if user is not None:
             auth_login(request, user)
             fname = user.first_name
-            return render(request,'payment.html',{"fname":fname})
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "payment.html",{"fname":fname})
         else:
-            messages.error(request, 'No user')
-            return redirect("index")
-   
-
-    return render(request ,"index.html")
+            messages.error(request, "Bad Credentials!!")
+            return redirect('index')
+    
+    return render(request, "index.html")
 
 
 
@@ -37,12 +36,33 @@ def signup(request):
         pass2 = request.POST.get("pass2")
 
 
+        if User.objects.filter(username=Username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('index')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('index')
+        
+        if len(Username)>20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('index')
+        
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('index')
+        
+        if not Username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!!")
+            return redirect('index')
+
+
         user = User.objects.create_user(Username, email, pass1)
         user.first_name = fname
         user.last_name = lname
         user.save()
 
-        messages.success(request, "Your account has been created")
+        messages.success(request, "Your Account has been created succesfully!! ")
         return redirect('index')
         
     return render(request ,"signup.html")
